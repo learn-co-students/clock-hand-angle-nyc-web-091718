20f0d9b721d1ed197186456c4834945ec1a98504
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-04 11:34:22 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,26 @@
+
 def clock_angle(time)
-    # code your solution here
+    #6 degrees per minute
+    #30 degrees per hour
+    hours_mins = time.split(":")
+    hours_mins = hours_mins.map {|x| x.to_i}
+    hours = hours_mins[0]
+  #  binding.pry
+    if hours_mins[1] != 0
+      minutes = hours_mins[1].to_f
+    else
+      minutes = hours_mins[1]
+    end
+
+    a = (hours * 60 + minutes)*0.5
+    b = (minutes * 6)
+    solution = a - b
+    solution = solution.abs
+    if solution >= 360
+      solution -= 360
+    elsif solution > 180 && solution < 360
+      solution -= 180
+    end
+    solution
+
 end
